d8567c634fab44b0dffb6f4dcca16bb7b1fd5b15
stetsonharding <stetson2001@msn.com>
2022-04-26 07:00:19 -0700

    Loading and error messages.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,16 @@ import "../src/App.css";
 
 function App() {
   //graphql data
-  const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+  const {
+    loading,
+    error,
+    data: { pokemons = [] } = {},
+  } = useQuery(GET_POKEMONS, {
     variables: { first: 120 },
   });
+
+  if (loading) return "Loading...";
+  if (error) return <pre>{error.message}</pre>;
 
   return (
     <main>
