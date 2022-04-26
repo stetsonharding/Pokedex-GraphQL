21085789871fdcbe853e73aedee58ddcd48c7107
stetsonharding <stetson2001@msn.com>
2022-04-26 07:40:32 -0700

    Added pagination to pokemons list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import PokemonsContainer from "./containers/PokemonsContainer";
 import { useQuery } from "@apollo/client";
 import { GET_POKEMONS } from "./graphql/fetch-pokemons";
 import "../src/App.css";
 
+import Pagination from "./components/Pagination";
+
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pokemonsPerPage, setPokemonsPerPage] = useState(16);
+
   //graphql data
   const {
     loading,
     error,
     data: { pokemons = [] } = {},
   } = useQuery(GET_POKEMONS, {
-    variables: { first: 120 },
+    variables: { first: 150 },
   });
+
+  const indexOfLastPokemons = currentPage * pokemonsPerPage;
+  const indexOfFirstPokemons = indexOfLastPokemons - pokemonsPerPage;
+  const currentPokemons = pokemons.slice(
+    indexOfFirstPokemons,
+    indexOfLastPokemons
+  );
+
+  //Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  console.log(currentPokemons);
 
   if (loading) return "Loading...";
   if (error) return <pre>{error.message}</pre>;
 
   return (
     <main>
-      <PokemonsContainer pokemons={pokemons} />
+      <PokemonsContainer pokemons={currentPokemons} />
+      <Pagination
+        pokemonsPerPage={pokemonsPerPage}
+        totalPokemons={pokemons.length}
+        paginate={paginate}
+      />
     </main>
   );
 }
